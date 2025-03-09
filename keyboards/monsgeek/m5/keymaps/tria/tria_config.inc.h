af31db6fefd6a278f577cf5481e4c913f7e8e74e
tri4ng1e <alex@naboko.tk>
2025-03-09 22:17:17 +0300

    Update main animation file

--- keyboards/monsgeek/m5/keymaps/tria/tria_config.inc.h
+++ keyboards/monsgeek/m5/keymaps/tria/tria_config.inc.h
@@ -5,9 +5,9 @@
 // RGB_MATRIX idle settings
 bool     tria_idle_enabled    = true;
 uint32_t tria_idle_timeout    = 10;    // s
-uint8_t  tria_idle_brightness = 75;
-uint16_t tria_idle_off_time   = 1000;  // ms
-uint16_t tria_idle_on_time    = 100;   // ms
+uint8_t  tria_idle_brightness = 75;    // out of 255
+uint16_t tria_idle_off_time   = 5000;  // ms
+uint16_t tria_idle_on_time    = 1000;  // ms
 
 HSV     tria_rgb_default_color = {110,255,200};
 uint8_t tria_rgb_default_speed = 120;
